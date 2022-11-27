4d38e5438135dae2354d9bd37ac23ce48b9c5fde
Eirik Vigeland <eirik.vigeland@vipps.no>
2022-11-27 08:56:40 +0100

    Counter

--- src/Counter.ts
+++ src/Counter.ts
@@ -1,10 +1,44 @@
-import { LitElement, html } from 'lit';
-import { customElement, state } from 'lit/decorators.js';
+import { LitElement, html, css } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
 
 @customElement('my-counter')
 class Counter extends LitElement {
+  public static styles = css`    
+    :host([color=blue]) button {
+      background: blue;
+      color: white;
+    }
+    
+    :host([color=red]) button {
+      background: red;
+      color: white;
+    }
+  `;
+  
+  @property({ type: Number, reflect: true })
+  public count = 0;
+  
+  private _onIncrement() {
+    if (this.count < 1_000_000) {
+      this.count++;
+      this.dispatchEvent(new CustomEvent('count', { detail: this.count }));
+    }
+  }
+
+  private _onDecrement() {
+    if (this.count > 0) {
+      this.count--;
+      this.dispatchEvent(new CustomEvent('count', { detail: this.count }));
+    }
+  }
 
   render() {
-    return html`Hello Counter!`;
+    return html`
+      <button type="button" @click="${this._onDecrement}">-</button>
+      <span>${this.count}</span>
+      <button type="button" @click="${this._onIncrement}">+</button>
+    `;
   }
 }
+
+export default Counter;
